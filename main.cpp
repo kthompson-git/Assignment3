afaf867509d11c9b60305db6c4e33e5a6a6e3650
Keith Thompson <cosc4568@program.cs.uh.edu>
2019-11-27 23:58:33 -0600

    fixed issue with overwriting zeroes

--- main.cpp
+++ main.cpp
@@ -90,6 +90,8 @@ void replaceOnes(char key, char code[])
   {
     if (code[i] == '1')
       code[i] = key;
+    else
+      code[i] = '\t';          
     i++;
   }
   return;
@@ -148,7 +150,7 @@ void combineMessages(char sharedMem[], char temp[])
   int i = 0, j = 0;
   while (sharedMem[i])
   {
-    if (sharedMem[i] == '0')
+    if (sharedMem[i] == '\t')
     {
       sharedMem[i] = temp[j];
       j++;
@@ -216,7 +218,7 @@ int main(int argc, char *argv[])
     }
   }
   
-  std::cout << "Decompressed file contents:\n" << sharedArray << std::endl;
+  std::cout << "Decompressed file contents:\n" << sharedArray << std::endl << std::endl;
   
   system("rm input*");
   sem_unlink(nameM);
